c3b36a5ce9870491ff0c6ca32ed440b3338d4983
D.Indu mahela <159713591+DanduIndumahela@users.noreply.github.com>
2024-12-29 18:44:24 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class While {
+    public static void main(String args[]){
+        //for while
+        //int num=0;
+        //while(num<=6){
+          //  System.out.println(num);
+            //num++;
+     //   }
+     //by using scanner class
+        System.out.println("enetr number");
+        Scanner sc=new Scanner (System.in);
+        int num=sc.nextInt();
+        while(num<=6){
+            System.out.println(num);
+            num++;
+            //System.out.println(n);
+        }
+    }
+}
